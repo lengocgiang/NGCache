1507869bec4bd193f1b3de415b9e7f729d3dd45a
Giang Le Ngoc <giangln@seesaa.vn>
2015-05-27 16:38:16 +0700

    Initial Commit

---
+++ NGCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NGCache
+//
+//  Created by Le Ngoc Giang on 5/27/15.
+//  Copyright (c) 2015 Le Ngoc Giang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
